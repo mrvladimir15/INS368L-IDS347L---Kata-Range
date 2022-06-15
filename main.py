7a673f889de1c130a61fce56a044e7dff5dfc4f0
mrvladimir15 <73143660+mrvladimir15@users.noreply.github.com>
2022-06-15 19:53:18 -0400

    Commit 3 pruebas y codigo fuente

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from ast import operator
+
+
 class Range():
     def __init__(self, operator1, number1, operator2, number2):
         self.opertor1 = operator1
         self.number1 = number1
         self.operator2 = operator2
         self.number2 = number2
+
+    def Equals(self, operator1, number1, operator2, number2):
+        if self.opertor1 == operator1 and self.number1 == number1 and self.operator2 == operator and self.number2==number2:
+            return True
+        else:
+            return False
--- test_range.py
+++ test_range.py
@@ -1,6 +1,6 @@
 from ast import operator
 import pytest
-from main import __init__
+from main import Range
 
 class TestRange1():
     operator1 = "["
@@ -9,16 +9,16 @@ class TestRange1():
     number2 = 6
     
     def test_t1(self):
-        assert Range.operator1 == "["
+        assert Range.Equals(self, "[", 2, ")", 6) == True
 
-class process():
-    def __init__(self, operator1, number1,operator2,number2):
-        self.first = number1
-        self.second = number2
-    def display (self)
-    print ("first number = " + str(self.first))
-    print ("first number = " + str(self.second))
-    print("additional of two numbers = "+ str(self.answer))
+# class process():
+#     def __init__(self, operator1, number1,operator2,number2):
+#         self.first = number1
+#         self.second = number2
+#     def display (self)
+#     print ("first number = " + str(self.first))
+#     print ("first number = " + str(self.second))
+#     print("additional of two numbers = "+ str(self.answer))
 
-    def calculate(self):
-        self.answer = self.first + self.second
+#     def calculate(self):
+#         self.answer = self.first + self.second
