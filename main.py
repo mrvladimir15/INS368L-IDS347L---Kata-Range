2e465936f6f86c1909e5f6c1fec150d310c227e3
mrvladimir15 <73143660+mrvladimir15@users.noreply.github.com>
2022-06-15 23:49:57 -0400

    Last commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from ast import operator
 
-
-class Range():
+class Range:
     def __init__(self, operator1, number1, operator2, number2):
         self.opertor1 = operator1
         self.number1 = number1
@@ -9,7 +8,61 @@ class Range():
         self.number2 = number2
 
     def Equals(self, operator1, number1, operator2, number2):
-        if self.opertor1 == operator1 and self.number1 == number1 and self.operator2 == operator and self.number2==number2:
+        if (
+            self.operator1 == operator1
+            and self.number1 == number1
+            and self.operator2 == operator2
+            and self.number2 == number2
+        ):
             return True
         else:
             return False
+
+    def IntegerRangeContains(self, num1, num2):
+        if self.operator1 == "[" and self.operator2 == "]":
+            if self.number1 <= num1 <= self.number2 and self.number1 <= num2 <= self.number2:
+                return True
+            else:
+                return False
+        elif self.operator1 == "[" and self.operator2 == ")":
+            if self.number1 <= num1 < self.number2 and self.number1 <= num2 < self.number2:
+                return True
+            else:
+                return False
+        elif self.operator1 == "(" and self.operator2 == ")":
+            if self.number1 < num1 < self.number2 and self.number1 < num2 < self.number2:
+                return True
+            else:
+                return False
+        elif self.operator1 == "(" and self.operator2 == "]":
+            if self.number1 < num1 <= self.number2 and self.number1 < num2 <= self.number2:
+                return True
+            else:
+                return False
+
+    def getAllPoints(self):
+        arrPoints = []
+        i = 0
+        if self.operator1 == "[" and self.operator2 == "]":
+            while self.number2 >= i:
+                arrPoints[i] = self.number1
+                i += 1
+                self.number1 += 1
+        elif self.operator1 == "[" and self.operator2 == ")":
+            while self.number2 > i:
+                arrPoints[i] = self.number1
+                i += 1
+                self.number1 += 1
+        elif self.operator1 == "(" and self.operator2 == ")":
+            while self.number2 > i:
+                self.number1 += 1
+                arrPoints[i] = self.number1
+                i += 1
+        elif self.operator1 == "(" and self.operator2 == "]":
+            while self.number2 >= i:
+                self.number1 += 1
+                arrPoints[i] = self.number1
+                i += 1
+        else:
+            return "Invalid operand"
+        return arrPoints
--- test_range.py
+++ test_range.py
@@ -9,8 +9,63 @@ class TestRange1():
     number2 = 6
     
     def test_t1(self):
-        assert Range.Equals(self, "[", 2, ")", 6) == True
+        assert Range.Equals(self, '[', 2, ')', 6) == True
 
+class TestRange2():
+    operator1 = "["
+    number1 = 3
+    operator2 = ")"
+    number2 = 5
+    
+    def test_t1(self):
+        assert Range.Equals(self, '[', 3, ')', 5) == True
+
+class TestRange3():
+    operator1 = "["
+    number1 = 2
+    operator2 = ")"
+    number2 = 10
+    
+    def test_t1(self):
+        assert Range.Equals(self, '[', 3, ')', 5) == False
+
+class TestRange4():
+    operator1 = "["
+    number1 = 2
+    operator2 = ")"
+    number2 = 5
+    
+    def test_t1(self):
+        assert Range.Equals(self, '[', 3, ')', 10) == False
+
+class TestRange5():
+    operator1 = "["
+    number1 = 3
+    operator2 = ")"
+    number2 = 5
+    
+    def test_t1(self):
+        assert Range.Equals(self, '[', 2, ')', 10) == False
+
+class TestRange6():
+    
+    operator1 = "["
+    number1 = 2
+    operator2 = ")"
+    number2 = 6
+    
+    def test_t1(self):
+        assert Range.getAllPoints(self) == [2, 3, 4, 5]
+
+class TestRange7():
+    operator1 = "["
+    number1 = 2
+    operator2 = ")"
+    number2 = 6
+    
+    def test_t1(self):
+        assert Range.IntegerRangeContains(self, 2, 4) == True
+    
 # class process():
 #     def __init__(self, operator1, number1,operator2,number2):
 #         self.first = number1
